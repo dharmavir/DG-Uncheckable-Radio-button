c120acd5c4da76abfe02a09272220f8eb3915c70
Dharmavirsinh Jhala <dharmavir@gmail.com>
2012-06-03 19:07:02 +0530

    made plugin code compatible for 1.6.x or newer
    As $(..).attr() has been deprecated since jquery 1.6 and new function
    prop() has been introduced. This commit removes usage of attr() and uses
    prop() in favor of continuing support for latest jquery versions.
    Though there was no issues reported or we did not found any problem with
    attr() but using prop() was a move for keeping it updated.

--- jquery.radio.checkbox.behavior.js
+++ jquery.radio.checkbox.behavior.js
@@ -1,8 +1,8 @@
 //##############################
 // jQuery Radio-buttons with Checkbox Behavior
 // By Dharmavirsinh Jhala - dharmavir@gmail.com
-// Last Updated: 13th Dec 09
-// Version: 1.2.5
+// Last Updated: 3rd June 2012
+// Version: 1.3.0
 /*
  USAGE:
 	$(document).ready(function(){
@@ -14,17 +14,17 @@ behaveLikeCheckbox: function(){
 	var radioStatus	=	false;
 	$.each($(this),function(){
 		// Set initial state of radio
-		this.radioStatus = $(this).attr("checked") || false;
+		this.radioStatus = $(this).prop("checked") || false;
 		$(this).data("status",this.radioStatus);
 		// Set mouse-over event callback
 		$(this).mouseover(function(){
-			this.radioStatus = $(this).attr("checked") || false;
+			this.radioStatus = $(this).prop("checked") || false;
 			$(this).data("status",this.radioStatus);
 		});
 		// Set on-click event callback
 		$(this).click(function(){
 			this.radioStatus = !$(this).data("status")
-			$(this).attr("checked",this.radioStatus);
+			$(this).prop("checked",this.radioStatus);
 			$(this).data("status",this.radioStatus);
 		});
 	});
